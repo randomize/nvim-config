2d3a4dcfe8947c0aba4072083e9640b6895b68e3
Eugene Mihailenco <emi@trackman.com>
2023-09-03 19:40:56 +0200

    Comment: Replaced mini with Comment.nvim

--- lua/randy/plugins/mini-comment.lua
+++ lua/randy/plugins/mini-comment.lua
@@ -1,17 +1,24 @@
 return {
-    -- "gc" to comment visual regions/lines
-    {
-        "echasnovski/mini.comment",
-        event = "VeryLazy",
-        opts = {
-            hooks = {
-                pre = function()
-                    require("ts_context_commentstring.internal").update_commentstring({})
-                end,
-            },
-        },
-        config = function(_, opts)
-            require("mini.comment").setup(opts)
-        end,
+  -- "gc" to comment visual regions/lines
+  -- {
+  --   "echasnovski/mini.comment",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     hooks = {
+  --       pre = function()
+  --         require("ts_context_commentstring.internal").update_commentstring({})
+  --       end,
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require("mini.comment").setup(opts)
+  --   end,
+  -- },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
     },
+    lazy = false,
+  }
 }
--- lua/randy/plugins/misc.lua
+++ lua/randy/plugins/misc.lua
@@ -2,4 +2,5 @@ return {
     { "shirk/vim-gas",      lazy = false }, -- gnu assembler syntax - ft=gas
     { "folke/neoconf.nvim", cmd = "Neoconf" },
     { "tridactyl/vim-tridactyl", lazy = false },
+    { "ron-rs/ron.vim", lazy = false },
 }
