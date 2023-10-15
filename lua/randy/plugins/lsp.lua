025fa463889ba6afa4efd4e0e27924baf48c872d
Eugene Mihailenco <emi@trackman.com>
2023-10-15 11:08:14 +0200

    LSP: Updates

--- lua/randy/plugins/lsp.lua
+++ lua/randy/plugins/lsp.lua
@@ -12,82 +12,81 @@ local root_dir = require("randy.lsp.root_dir")
 -- end,
 
 local custom_mappings_on_attach = function(client, bufnr)
-
     if client.name == "omnisharp" then
         print("client is omnisharp")
         client.server_capabilities.semanticTokensProvider = {
-          full = vim.empty_dict(),
-          legend = {
-            tokenModifiers = { "static_symbol" },
-            tokenTypes = {
-              "comment",
-              "excluded_code",
-              "identifier",
-              "keyword",
-              "keyword_control",
-              "number",
-              "operator",
-              "operator_overloaded",
-              "preprocessor_keyword",
-              "string",
-              "whitespace",
-              "text",
-              "static_symbol",
-              "preprocessor_text",
-              "punctuation",
-              "string_verbatim",
-              "string_escape_character",
-              "class_name",
-              "delegate_name",
-              "enum_name",
-              "interface_name",
-              "module_name",
-              "struct_name",
-              "type_parameter_name",
-              "field_name",
-              "enum_member_name",
-              "constant_name",
-              "local_name",
-              "parameter_name",
-              "method_name",
-              "extension_method_name",
-              "property_name",
-              "event_name",
-              "namespace_name",
-              "label_name",
-              "xml_doc_comment_attribute_name",
-              "xml_doc_comment_attribute_quotes",
-              "xml_doc_comment_attribute_value",
-              "xml_doc_comment_cdata_section",
-              "xml_doc_comment_comment",
-              "xml_doc_comment_delimiter",
-              "xml_doc_comment_entity_reference",
-              "xml_doc_comment_name",
-              "xml_doc_comment_processing_instruction",
-              "xml_doc_comment_text",
-              "xml_literal_attribute_name",
-              "xml_literal_attribute_quotes",
-              "xml_literal_attribute_value",
-              "xml_literal_cdata_section",
-              "xml_literal_comment",
-              "xml_literal_delimiter",
-              "xml_literal_embedded_expression",
-              "xml_literal_entity_reference",
-              "xml_literal_name",
-              "xml_literal_processing_instruction",
-              "xml_literal_text",
-              "regex_comment",
-              "regex_character_class",
-              "regex_anchor",
-              "regex_quantifier",
-              "regex_grouping",
-              "regex_alternation",
-              "regex_text",
-              "regex_self_escaped_character",
-              "regex_other_escape",
+            full = vim.empty_dict(),
+            legend = {
+                tokenModifiers = { "static_symbol" },
+                tokenTypes = {
+                    "comment",
+                    "excluded_code",
+                    "identifier",
+                    "keyword",
+                    "keyword_control",
+                    "number",
+                    "operator",
+                    "operator_overloaded",
+                    "preprocessor_keyword",
+                    "string",
+                    "whitespace",
+                    "text",
+                    "static_symbol",
+                    "preprocessor_text",
+                    "punctuation",
+                    "string_verbatim",
+                    "string_escape_character",
+                    "class_name",
+                    "delegate_name",
+                    "enum_name",
+                    "interface_name",
+                    "module_name",
+                    "struct_name",
+                    "type_parameter_name",
+                    "field_name",
+                    "enum_member_name",
+                    "constant_name",
+                    "local_name",
+                    "parameter_name",
+                    "method_name",
+                    "extension_method_name",
+                    "property_name",
+                    "event_name",
+                    "namespace_name",
+                    "label_name",
+                    "xml_doc_comment_attribute_name",
+                    "xml_doc_comment_attribute_quotes",
+                    "xml_doc_comment_attribute_value",
+                    "xml_doc_comment_cdata_section",
+                    "xml_doc_comment_comment",
+                    "xml_doc_comment_delimiter",
+                    "xml_doc_comment_entity_reference",
+                    "xml_doc_comment_name",
+                    "xml_doc_comment_processing_instruction",
+                    "xml_doc_comment_text",
+                    "xml_literal_attribute_name",
+                    "xml_literal_attribute_quotes",
+                    "xml_literal_attribute_value",
+                    "xml_literal_cdata_section",
+                    "xml_literal_comment",
+                    "xml_literal_delimiter",
+                    "xml_literal_embedded_expression",
+                    "xml_literal_entity_reference",
+                    "xml_literal_name",
+                    "xml_literal_processing_instruction",
+                    "xml_literal_text",
+                    "regex_comment",
+                    "regex_character_class",
+                    "regex_anchor",
+                    "regex_quantifier",
+                    "regex_grouping",
+                    "regex_alternation",
+                    "regex_text",
+                    "regex_self_escaped_character",
+                    "regex_other_escape",
+                },
             },
-          },
-          range = true,
+            range = true,
         }
     end
 
@@ -125,13 +124,13 @@ local custom_mappings_on_attach = function(client, bufnr)
     nmap("<space>G", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
     nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
     nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-    nmap("<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, "[W]orkspace [L]ist Folders")
+    nmap("<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+        "[W]orkspace [L]ist Folders")
 
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
         vim.lsp.buf.format()
     end, { desc = "Format current buffer with LSP" })
-
 end
 
 
@@ -140,7 +139,7 @@ return {
     {
         "VonHeikemen/lsp-zero.nvim",
         dependencies = {
-            {"neovim/nvim-lspconfig", branch="master"},
+            { "neovim/nvim-lspconfig", branch = "master" },
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             "hrsh7th/nvim-cmp",
@@ -154,39 +153,62 @@ return {
             "rafamadriz/friendly-snippets",
             "Hoffs/omnisharp-extended-lsp.nvim",
             { "lukas-reineke/lsp-format.nvim", config = true },
+            "b0o/schemastore.nvim", -- collection of json schemas
         },
         lazy = false,
         config = function()
-            local lsp = require("lsp-zero")
+            local lsp_zero = require("lsp-zero")
             -- print('configure lsp doing....')
-            lsp.preset("recommended")
-            lsp.on_attach(function(client, bufnr) custom_mappings_on_attach(client, bufnr) end)
-            lsp.nvim_workspace()
+            lsp_zero.preset("recommended")
+            lsp_zero.on_attach(function(client, bufnr) custom_mappings_on_attach(client, bufnr) end)
 
-            lsp.ensure_installed({
+            local lspconfig = require('lspconfig')
+
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
                 'tsserver',
                 'rust_analyzer',
                 'lua_ls',
+                'jsonls',
+                'bashls',
                 --'azure_pipelines_ls'
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        lspconfig.lua_ls.setup(lua_opts)
+                    end,
+                    lspconfig.jsonls.setup({
+                        settings = {
+                            json = {
+                                schema = require('schemastore').json.schemas(),
+                                validate = { enable = true },
+                            }
+                        }
+                    }),
+                }
             })
 
-            lsp.configure('azure_pipelines_ls', {
-                force_setup = true,
-                cmd = {'node', '/home/randy/.node_modules/lib/node_modules/azure-pipelines-language-server/out/server.js', '--stdio'},
-                settings = {
-                   yaml = {
-                       schemas = {
-                           ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
-                               "/azure-*.yml",
-                               "Azure-Pipelines/**/*.y*l",
-                               "Pipelines/*.y*l",
-                           },
-                       },
-                   },
-               },
-            })
+            -- lsp_zero.configure('azure_pipelines_ls', {
+            --     force_setup = true,
+            --     cmd = { 'node',
+            --         '/home/randy/.node_modules/lib/node_modules/azure-pipelines-language-server/out/server.js', '--stdio' },
+            --     settings = {
+            --         yaml = {
+            --             schemas = {
+            --                 ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+            --                     "/azure-*.yml",
+            --                     "Azure-Pipelines/**/*.y*l",
+            --                     "Pipelines/*.y*l",
+            --                 },
+            --             },
+            --         },
+            --     },
+            -- })
 
-            lsp.configure('omnisharp', {
+            lsp_zero.configure('omnisharp', {
                 root_dir = root_dir,
                 on_attach = function(client, bufnr) custom_mappings_on_attach(client, bufnr) end,
                 handlers = {
@@ -194,45 +216,26 @@ return {
                 },
             })
 
-            lsp.configure('lua_ls', {
-                on_attach = function(client, bufnr)
-                    print('hello lua_ls')
-                end,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global
-                            globals = {'vim'},
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                        -- Do not send telemetry data containing a randomized but unique identifier
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-
-            lsp.setup()
+            lsp_zero.setup()
             vim.diagnostic.config { virtual_text = true, update_in_insert = false }
 
-            lsp.new_server({
-              name = 'randy-packs-lsp',
-              cmd = {'/home/randy/Documents/rdev/lsp-hello/tower-lsp-boilerplate/target/debug/nrs-language-server'},
-              filetypes = {'randypacks'},
-              root_dir = function()
-                -- return lsp.dir.find_first({'some-config-file'})
-                return "/home/randy/dots/nfo"
-              end
+            lsp_zero.new_client({
+                name = 'randy-packs-lsp',
+                cmd = { '/home/randy/Documents/rdev/lsp-hello/tower-lsp-boilerplate/target/debug/nrs-language-server' },
+                filetypes = { 'randypacks' },
+                root_dir = function()
+                    -- return lsp.dir.find_first({'some-config-file'})
+                    return "/home/randy/dots/nfo"
+                end
             })
 
+            -- In v3 lsp-zero need to explicitly add format of item with source label
+            local cmp = require('cmp')
+            local cmp_format = lsp_zero.cmp_format()
+
+            cmp.setup({
+                formatting = cmp_format,
+            })
         end
     }
 }
