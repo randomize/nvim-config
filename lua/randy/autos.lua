f280b0bd989d092396df9e60a8906e0f80252008
Eugene Mihailenco <emi@trackman.com>
2024-09-14 21:15:14 +0200

    Python: Adding autos to show special characters
    Adding some listchars for cases when python script is using tabs to make
    sure I see them

--- lua/randy/autos.lua
+++ lua/randy/autos.lua
@@ -8,3 +8,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.opt_local.list = true
+    vim.opt_local.listchars = {
+      space = '·',
+      tab = '→ ',
+      trail = '·',
+      extends = '❯',
+      precedes = '❮',
+      nbsp = '␣',
+    }
+  end,
+})
