84c5fc7bd84f614b3cb0592ad3cc2bbc3a007ce4
Eugene Mihailenco <emi@trackman.com>
2024-11-08 12:24:10 +0100

    SessionManager: Disable restore at startup properly

--- lua/randy/plugins/neovim-session-manager.lua
+++ lua/randy/plugins/neovim-session-manager.lua
@@ -8,12 +8,13 @@ return {
         config = function(_, opts)
             local Path = require("plenary.path")
             opts.sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions") -- The directory where the session files will be saved.
+            -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+            opts.autoload_mode = require("session_manager.config").AutoloadMode.Disabled
             require("session_manager").setup(opts)
         end,
         opts = {
             path_replacer = "__", -- The character to which the path separator will be replaced for session files.
             colon_replacer = "++", -- The character to which the colon symbol will be replaced for session files.
-            -- autoload_mode = require("session_manager.config").AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
             autosave_last_session = true, -- Automatically save last session on exit and on session switch.
             autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
             autosave_ignore_dirs = {}, -- A list of directories where the session will not be autosaved.
