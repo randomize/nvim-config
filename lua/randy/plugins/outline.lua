a7abf4b7358ab4006db6c29c48cfa6eab9e90578
Eugene Mihailenco <emi@trackman.com>
2025-07-18 23:40:57 +0200

    Updates: More forks switched

--- lua/randy/plugins/outline.lua
+++ lua/randy/plugins/outline.lua
@@ -1,12 +1,12 @@
-return {
-    "hedyhli/outline.nvim",
-    config = function()
-        -- Example mapping to toggle outline
-        vim.keymap.set("n", "<leader>q", "<cmd>Outline<CR>",
-            { desc = "Toggle Outline" })
-
-        require("outline").setup {
-            -- Your setup opts here (leave empty to use defaults)
-        }
-    end,
+return { {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = { -- Example mapping to toggle outline
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+},
 }
