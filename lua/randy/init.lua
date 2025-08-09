1be6c5d2a3f1444a1a81ece65def1e21d978f3eb
Eugene Mihailenco <emi@trackman.com>
2025-08-09 12:39:53 +0200

    Updates: Lazy better bootstrapping

--- lua/randy/init.lua
+++ lua/randy/init.lua
@@ -4,15 +4,18 @@ require('randy.autos')
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,12 +24,11 @@ require('lazy').setup({
         { import = "randy.plugins" },
     },
     defaults = {
-        lazy = true,   -- determines if every plugin is lazy-loaded by default
-        -- version = "*", -- try installing the latest stable version for plugins that support semver
+        lazy = true,
     },
     install = { colorscheme = { "tokyonight", } },
-    checker = { enabled = false }, -- automatically check for plugin updates
-    change_detection = { enabled = true, notify = false },
+    checker = { enabled = false },
+    change_detection = { enabled = true, notify = true },
     performance = {
         rtp = {
             -- disable some rtp plugins
